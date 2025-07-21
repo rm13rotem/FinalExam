50ec648674f44ccb7d2c11b761a8b0085b9d6967
Rotem Meron <rm13rotem@gmail.com>
2025-07-21 20:49:33 +0300

    variables

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -9,3 +9,25 @@ variable "my_ip_cidr" {
 
 # and running ... terraform apply -var="my_ip_cidr=35.86.136.73/32"
 
+#had some serious trouble with part 2 so I reset the SSH keys
+
+variable "ssh_key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
+
+variable "public_key_path" {
+  description = "Path to the public key file"
+  type        = string
+}
+
+variable "private_key_path" {
+  description = "Path to the private key file (used for SSH provisioner)"
+  type        = string
+}
+
+variable "my_ip_cidr" {
+  description = "Your current IP address in CIDR format"
+  type        = string
+}
+
